dcd83e6e6db5855e8018169bfcbbf6048fc4631d
lengors <24527258+lengors@users.noreply.github.com>
2025-01-12 23:30:04 +0000

    👷 Add `junit` dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ repositories {
 
 dependencies {
     dokkaHtmlPlugin("org.jetbrains.dokka:versioning-plugin:$dokkaVersioningPluginVersion")
+    testImplementation(kotlin("test"))
 }
 
 kotlin.compilerOptions {
@@ -59,57 +60,63 @@ ktlint {
     version = ktlintVersion
 }
 
-tasks.withType<DokkaGeneratePublicationTask> {
-    val docsDir =
-        project
-            .findProperty("dokkaOutputDir")
-            ?.toString()
-            ?.let(Paths::get)
-            ?.toFile()
-            ?: projectDir
-                .resolve("build")
-                .resolve("dokka")
-                .resolve("generated")
-    val currentVersion = "${project.version}"
-    val versionsDir =
-        project
-            .findProperty("dokkaVersionsDir")
-            ?.toString()
-            ?.let(Paths::get)
-            ?.toFile()
-    doFirst {
-        versionsDir?.let {
-            docsDir
-                .resolve("older")
-                .takeIf { olderDir -> olderDir.isDirectory }
-                ?.renameTo(it)
-            docsDir
-                .resolve("version.json")
-                .takeIf { versionFile -> versionFile.isFile }
-                ?.also { versionFile ->
-                    jacksonObjectMapper()
-                        .readTree(versionFile)
-                        .get("version")
-                        .textValue()
-                        ?.let { version -> docsDir.renameTo(it.resolve(version)) }
-                }
+tasks {
+    withType<DokkaGeneratePublicationTask> {
+        val docsDir =
+            project
+                .findProperty("dokkaOutputDir")
+                ?.toString()
+                ?.let(Paths::get)
+                ?.toFile()
+                ?: projectDir
+                    .resolve("build")
+                    .resolve("dokka")
+                    .resolve("generated")
+        val currentVersion = "${project.version}"
+        val versionsDir =
+            project
+                .findProperty("dokkaVersionsDir")
+                ?.toString()
+                ?.let(Paths::get)
+                ?.toFile()
+        doFirst {
+            versionsDir?.let {
+                docsDir
+                    .resolve("older")
+                    .takeIf { olderDir -> olderDir.isDirectory }
+                    ?.renameTo(it)
+                docsDir
+                    .resolve("version.json")
+                    .takeIf { versionFile -> versionFile.isFile }
+                    ?.also { versionFile ->
+                        jacksonObjectMapper()
+                            .readTree(versionFile)
+                            .get("version")
+                            .textValue()
+                            ?.let { version -> docsDir.renameTo(it.resolve(version)) }
+                    }
+            }
+        }
+        outputDirectory = docsDir
+        generator.dokkaSourceSets.configureEach {
+            sourceLink {
+                localDirectory = projectDir.resolve("src")
+                remoteUrl = URI.create("https://github.com/lengors/${project.name}/tree/$currentVersion/src")
+                remoteLineSuffix = "#L"
+            }
+        }
+        generator.pluginsConfiguration.versioning {
+            olderVersionsDir = versionsDir
+            version = currentVersion
+        }
+        generator.pluginsConfiguration.html {
+            customAssets.from(file("dokka/assets/github.svg"))
+            customStyleSheets.from(file("dokka/styleSheets/custom.css"))
+            templatesDir = file("dokka/templates")
         }
     }
-    outputDirectory = docsDir
-    generator.dokkaSourceSets.configureEach {
-        sourceLink {
-            localDirectory = projectDir.resolve("src")
-            remoteUrl = URI.create("https://github.com/lengors/${project.name}/tree/$currentVersion/src")
-            remoteLineSuffix = "#L"
-        }
-    }
-    generator.pluginsConfiguration.versioning {
-        olderVersionsDir = versionsDir
-        version = currentVersion
-    }
-    generator.pluginsConfiguration.html {
-        customAssets.from(file("dokka/assets/github.svg"))
-        customStyleSheets.from(file("dokka/styleSheets/custom.css"))
-        templatesDir = file("dokka/templates")
+
+    withType<Test> {
+        useJUnitPlatform()
     }
 }

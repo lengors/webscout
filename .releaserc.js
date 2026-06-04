8d234f4ec932f9193158da0d6dbc95de283fe129
lengors <24527258+lengors@users.noreply.github.com>
2026-06-04 01:32:50 +0100

    👷 Allow workflow dispatch for feature and bug branches

---
+++ .releaserc.js
@@ -0,0 +1,43 @@
+const branch = process.env.GITHUB_REF_NAME;
+
+const isPreview =
+    branch.startsWith("feat/") ||
+    branch.startsWith("bug/");
+
+module.exports = {
+    branches: [
+        "main",
+        {name: "beta", prerelease: true},
+        {name: "alpha", prerelease: true},
+        {name: "dev", prerelease: true},
+        {name: "feat/**", prerelease: "preview.${name.replace(/^feat\\//, '').replace(/\\//g, '-')}"},
+        {name: "bug/**", prerelease: "preview.${name.replace(/^bug\\//, '').replace(/\\//g, '-')}"},
+    ],
+    plugins: isPreview
+        ? [
+            "semantic-release-gitmoji",
+            ["@semantic-release/changelog", {
+                changelogFile: "CHANGELOG.md",
+            }],
+            ["@semantic-release/exec", {
+                prepareCmd: "./gradlew setVersion -PnewVersion=${nextRelease.version}",
+            }],
+            ["@semantic-release/github", {
+                releasedLabels: ["released on @preview"],
+            }],
+        ]
+        : [
+            "semantic-release-gitmoji",
+            ["@semantic-release/changelog", {
+                changelogFile: "CHANGELOG.md",
+            }],
+            ["@semantic-release/exec", {
+                prepareCmd: "./gradlew setVersion -PnewVersion=${nextRelease.version}",
+            }],
+            "@semantic-release/github",
+            ["@semantic-release/git", {
+                assets: ["CHANGELOG.md", "gradle.properties"],
+                message: "🔖 Update `gradle.properties` to `${nextRelease.version}` [skip release]\n\n${nextRelease.notes}",
+            }],
+        ],
+};
